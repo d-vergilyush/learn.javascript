3701a72593d1aeda7b5a45b3768159d6a4892022
Dana <vergilyush@gmail.com>
2018-04-09 12:17:07 +0300

    last commit

--- lesson_9_2/src/script.js
+++ lesson_9_2/src/script.js
@@ -1,19 +1,29 @@
+//1. конструктор
+
+// function User (name, surname) {
+// this.value = document.getElementById('age').value;
+// this.showUser = function() {
+// 		alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// 	}
+// }
+// let ivan = new User('Иван', 'Петриченко');  
+// ivan.showUser();
+
+// 2. метод объекта
+
+// let user = {
+// 	value: document.getElementById('age').value,
+// 	showUser: function(name, surname) {
+// 		alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// 	}
+// }
+// user.showUser('Иван', 'Петриченко');
+
+// 3. явное указание this 
 
 let age = document.getElementById('age');
-function User (name, surname) {
-this.name = name;
-this.surname = surname;
-this.value = age.value;
-this.showUser = function() {
-		alert("Пользователь " + this.surname + " " + this.name + ", его возраст " + this.value);
-	}
+function showUser(surname, name) {
+         alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
-let ivan = new User('Иван', 'Петриченко');  
-ivan.showUser();
+showUser.call(age, 'Никита', 'Трофимов');
 
-
-// let age = document.getElementById('age');
-// function showUser(surname, name) {
-//          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-// }
-// showUser.call(age, 'Никита', 'Трофимов');
